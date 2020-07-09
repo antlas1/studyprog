2396b7adf988e853955b38b4f63f7c1b870a328c
antlas <anton_lastochkin@mail.ru>
2020-07-09 20:41:09 +0300

    refactor user model gen, add table template

--- user_model_gen/generator.py
+++ user_model_gen/generator.py
@@ -8,24 +8,16 @@ import re
 import codecs
 import argparse
 import json
-
 from jinja2 import Environment, PackageLoader, select_autoescape
+from abc import ABC, abstractmethod
 
-#def render_test(file_name: str, markdown_content: str, out_dir: str, ext: str, wrapper: str) -> None:
-#    """Renderizar examen en formato Markdown a un HTML."""
-#
-#    extensions = ["tables", "app.extensions.checkbox", "app.extensions.radio", "app.extensions.textbox", "app.extensions.codebox"]
-#
-#    html = markdown.markdown(markdown_content, extensions=extensions, output_format="html5")
-#    env = Environment(loader=PackageLoader('app', 'static'), autoescape=select_autoescape(['html', 'xml']))
-#
-#    javascript = env.get_template('app.js').render()
-#
-#    test_html = env.get_template('base.html').render(content=html, javascript=javascript)
-#    test_html = env.get_template(wrapper).render(content=test_html)
-#
-#    with io.open(f"{out_dir}/{file_name[:-2]}{ext}", "w+", encoding='UTF-8') as f:  # create final file
-#        f.write(test_html)
+def render_model(model_json_content: str, out_fname: str, wrapper: str) -> None:
+    env = Environment(loader=PackageLoader('app', 'static'), autoescape=select_autoescape(['html', 'xml']))
+
+    out_html = env.get_template(wrapper).render(model=model_json_content)
+
+    with io.open(f"{out_fname}", "w+", encoding='UTF-8') as f:
+        f.write(out_html)
 
 def parse_md_list(content_lines):
    res_map={}
@@ -55,42 +47,90 @@ def parse_md_list(content_lines):
                        childs[i] = childs[i].strip()
                     rule_body = rule_body[:pos_st_childs]
                  #print(rule_id+':'+rule_body+' childs:'+str(childs))
-                 res_map[rule_id] = {'body':rule_body, 'childs':childs}
+                 res_map[rule_id] = {'body':rule_body.strip(), 'childs':childs}
          
    return res_map
    
-def get_connected(graph, start, connected, treant_obj):
-    #add genereta treant json:
-    #nodeStructure: {
-    #    text: {
-    #        name: "Djokovic, Novak",
-    #        desc: "4-6, 6-2, 6-2"
-    #    },
-    #    children: [
-    #        {
-    #            text: {
-    #                name: "Djokovic, Novak",
-    #                desc: "4-6, 6-2, 6-2"
-    #            },
-    #            children: [
-    #                {
-    #                    text: {
-    #                        name: "Djokovic, Novak",
-    #                        desc: "4-6, 6-1, 6-4"
-    #                    },
-    #                    children: [
-    #                        {
-    if not start in graph:
-       print('Warning: wrong child id='+start)
-       return None
-    if not start in connected:
-       connected.append(start)
-       #print('Connect '+start)
-    if len(graph[start]['childs']) == 0:
-        return None
-    for node in graph[start]['childs']:
-        get_connected(graph, node, connected, treant_obj)
-    return None
+   
+class ModelIterator(ABC):
+    """
+    Интерфейс итератора позволяет обходить дерево модели, представленное в виде списка
+    """
+
+    @abstractmethod
+    def iterate(self, graph, start) -> None:
+        pass
+        
+        
+class ConnectedFilterIterator(ModelIterator):
+    def __init__(self):
+        self._connected_id = []
+        self._connected_model = {}
+        
+    def iterate(self, graph, start) -> None:  
+        if not start in graph:
+           print('Warning: wrong child id='+start)
+        if not start in self._connected_id:
+           self._connected_id.append(start)
+           self._connected_model[start] = graph[start]
+           if len(graph[start]['childs']) == 0:
+               return
+           for node in graph[start]['childs']:
+                self.iterate(graph, node)
+        return
+
+    def clear(self):
+        self._connected = []
+        self._connected_model = {}
+        
+    def connected_id(self):
+        return self._connected_id
+        
+    def connected_model(self):
+        return self._connected_model
+        
+class TreantViewIterator(ModelIterator):
+    def __init__(self):
+        self._treant_obj = {}
+        
+    def iterate(self, graph, start) -> None:  
+        self._treant_obj = self._iterate_with_return(graph, start)
+        
+    def json_repr(self):
+        return json.dumps(self._treant_obj, ensure_ascii=False);
+       
+    def _iterate_with_return(self, graph, start) -> None: 
+       if not start in graph:
+           return None
+       node_hier = { 'text': { 'name' : start, 'desc' : graph[start]['body'] }, 'children' : [] }
+       if len(graph[start]['childs']) == 0:
+           return node_hier
+       for node in graph[start]['childs']:
+            node2 = self._iterate_with_return(graph, node)
+            if node2 is not None:
+               node_hier['children'].append(node2)
+       return node_hier  
+
+class TableViewIterator(ModelIterator):
+    def __init__(self):
+        self._table = []
+        
+    def iterate(self, graph, start) -> None:  
+       if not start in graph:
+           return
+       self._table.append({'sid' : start, 'desc' : graph[start]['body'], 'childs' : ','.join(graph[start]['childs']) })
+       if len(graph[start]['childs']) == 0:
+           return
+       for node in graph[start]['childs']:
+            self.iterate(graph, node)
+       return
+
+    def clear(self):
+        self._table = []
+        
+    def json_repr(self):
+        return json.dumps(self._table, ensure_ascii=False);
+
 
 if __name__ == "__main__":
 
@@ -114,7 +154,8 @@ if __name__ == "__main__":
        procedure_map = parse_md_list(f.readlines())
     with io.open(f"{args.skills}", "r", encoding='UTF-8') as f:
        skills_map = parse_md_list(f.readlines())
-       
+    
+    #полная модель, в сыром виде    
     model = {
     'facts': facts_map,
     'procedures': procedure_map,
@@ -122,15 +163,28 @@ if __name__ == "__main__":
     }
     
     #поиск подключенных узлов
-    conn_facts = []
-    treant_obj = {}
-    get_connected(facts_map,'root',conn_facts,treant_obj)
+    fact_it = ConnectedFilterIterator()
+    fact_it.iterate(facts_map,'root')
+    conn_facts = fact_it.connected_id()
+    
     disconn_facts = []
     for key in facts_map:
        if not key in conn_facts:
           disconn_facts.append(key)
+          
+    conn_fact_model = fact_it.connected_model()
+    #вид графа
+    treant_it = TreantViewIterator()
+    treant_it.iterate(conn_fact_model,"root")
+    #вид таблицы
+    table_it = TableViewIterator()
+    table_it.iterate(conn_fact_model,"root")
+    #print(table_it.json_repr())
     
-    print('Disconnected facts: '+','.join(disconn_facts))
+    render_model(table_it.json_repr(),'../site/model.html','wrapper.html')
+    
+    #print(treant_it.json_repr())
+    #print('Disconnected facts: '+','.join(disconn_facts))
     
     #дамп для исследования
     with io.open(f"{args.json}", "w+", encoding='UTF-8') as f:
