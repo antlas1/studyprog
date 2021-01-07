58743cec8e979dd318dacc82df99f0cccc4ec6e9
anlastochkin <anton_lastochkin@mail.ru>
2021-01-07 18:55:37 +0300

    upd

--- scripts/robo/robo.py
+++ scripts/robo/robo.py
@@ -1,33 +1,34 @@
-#Meet Robo: your friend
-#+идеи: https://progkids.com/blog/five-python-projects
 # Обработка ответа
-def response(user_response):
-    #robo_response=''
-    #sent_tokens.append(user_response)
-    #TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    #tfidf = TfidfVec.fit_transform(sent_tokens)
-    #vals = cosine_similarity(tfidf[-1], tfidf)
-    #idx=vals.argsort()[0][-2]
-    #flat = vals.flatten()
-    #flat.sort()
-    #req_tfidf = flat[-2]
-    #if(req_tfidf==0):
-    #    robo_response=robo_response+"I am sorry! I don't understand you"
-    #    return robo_response
-    #else:
-    #    robo_response = robo_response+sent_tokens[idx]
-    #    return robo_response
-    return "Пока нет ответа"
+def response1(word):
+	return "Одно непонятное слово"
+	
+def response2(word1,word2):
+	return "Два непонятных слова"
+	
+def response3(word1,word2,word3):
+	return "Три непонеятных слова"
 
-
+#Основная программа. Не менять!
 flag=True
-print("РОБО: Меня зовут РОБО. Я буду вашим личным ассистентом. Разве не здорово? Если хотите выйти, то напишите \"пока\"")
+print("РОБО: Меня зовут РОБО. Я буду вашим личным ассистентом. Разве не здорово? Если хотите выйти, то напишите \"пока\"\n>", end='')
 while(flag==True):
-    user_response = input()
-    user_response=user_response.lower().strip()
-    if(user_response != 'пока'):
-        resp = response(user_response)
-        print("РОБО: "+response(user_response))
-    else:
-        flag=False
-        print("РОБО: Пока! Берегите себя.") 
+	user_response = input()
+	user_response=user_response.lower().strip()
+	if(user_response != 'пока'):
+		words_resp = user_response.split(' ')
+		if len(words_resp)>3:
+			print("РОБО: очень много слов, я понимаю от одного до трёх. Попробуйте еще раз.\n>", end='')
+		elif len(words_resp)==0 or (len(words_resp)==1 and len(words_resp[0])==0):
+			print("РОБО: ну, помолчим.\n>", end='')
+		else:
+			resp = "Неизвестно"
+			if len(words_resp)==1:
+				resp = response1(words_resp[0])
+			elif len(words_resp)==2:
+				resp = response2(words_resp[0],words_resp[1])
+			else:
+				resp = response3(words_resp[0],words_resp[1],words_resp[2])
+			print("РОБО: "+resp+"\n>", end='')
+	else:
+		flag=False
+		print("РОБО: Пока! Берегите себя.") 
